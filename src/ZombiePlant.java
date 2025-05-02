89a2ae8b6cb218b088d76f70f71f631e49c8bbc5
roninst2027 <roninst2027@student.cis.edu.hk>
2025-05-02 06:15:24 +0000

    New

---
+++ src/ZombiePlant.java
@@ -0,0 +1,28 @@
+public class ZombiePlant{
+    private int potency;
+    private int treatmentsNeeded;
+
+public ZombiePlant(int potency, int treatmentsNeeded) {
+        this.potency = potency;
+        this.treatmentsNeeded = treatmentsNeeded;
+    }
+
+    public int getPotency() {
+        return potency;
+    }
+
+    public int treatmentsNeeded() {
+        return treatmentsNeeded;
+    }
+
+    public boolean isDangerous() {
+        return treatmentsNeeded > 0;
+    }
+
+    public void treat(int treatmentPotency) {
+        if (treatmentPotency >= potency) {
+            treatmentsNeeded--;
+        }
+    }
+
+}
